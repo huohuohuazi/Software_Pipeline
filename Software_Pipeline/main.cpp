08bd233fb48179c33a63d498217f05fbe554be59
huohuo <2401722744@qq.com>
2022-07-28 08:14:43 +0800

    更新Shader类

--- Software_Pipeline/main.cpp
+++ Software_Pipeline/main.cpp
@@ -5,7 +5,7 @@
 
 #include <GL/glut.h>
 #include <GLFW/glfw3.h>
-
+#include"Shader.h"
 
 using namespace std;
 
@@ -45,8 +45,11 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, true);
 }
 
+
 int main(int argc, char* argv[])
 {
+#pragma region 初始化窗口
+    //初始化窗口
     glutInit(&argc, argv);
     if (!glfwInit())
     {
@@ -63,7 +66,9 @@ int main(int argc, char* argv[])
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//注册回调函数
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         cout << "Failed to initialize GLAD" << endl;
@@ -72,27 +77,81 @@ int main(int argc, char* argv[])
 
     DisplayDeviceInfo();
 
-    glViewport(0, 0, 1280, 720);
-    //注册回调函数
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+#pragma endregion
 
-    //主循环
-    while (!glfwWindowShouldClose(window))//检查glfw是否被要求退出
+#pragma region 指定VAO VBO 与着色器
+
+    
+    //绘制信息
+    float vertices[] = {
+        // 位置              // 颜色
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+    };
+
+    //新建VAO VBO对象
+    unsigned int VBO, VAO;
+    int index = 1;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    //将VBO添加至BUUFFER中
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //加载shader
+    Shader ourShader("./Shaders/simpleTriangle.vert", "./Shaders/simpleTriangle.frag");
+
+
+    // VAO设置
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    //对于location=0,vec3,type=float,normalize=false,补偿=3*float,强制类型转换
+    glEnableVertexAttribArray(0);//启用顶点属性
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)((3 * sizeof(float))));
+    glEnableVertexAttribArray(1);//启用顶点属性
+
+#pragma endregion
+
+#pragma region 绘制循环
+    while (!glfwWindowShouldClose(window))
     {
-        //输入
+        // 输入
         processInput(window);
 
-        //渲染指令
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);//填充颜色
+        // 渲染
+        // 清除颜色缓冲
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
 
-        glClear(GL_COLOR_BUFFER_BIT);//清理颜色缓冲
+        // 更新uniform颜色
+        //float timeValue = glfwGetTime();
+        //float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        //int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
+        // 给shader传入数据
+        ourShader.setFloat("time", glfwGetTime() * 2);
+        glBindVertexArray(VAO);
 
-        //检查并调用事件，交换缓冲
+        ourShader.use();
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);//0是顶点索引，3是顶点长
+
+        // 交换缓冲并查询IO事件
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+#pragma endregion
+
+    // 释放资源
+    glDeleteVertexArrays(index, &VAO);
+    glDeleteBuffers(index, &VBO);
+
 
     glfwTerminate();
     cout << "Done" << endl;
